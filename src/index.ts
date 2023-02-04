a966f7591e5679234f96be1e5f9039f986011934
Alexey <praesidios@gmail.com>
2023-02-04 21:11:41 +0300

    feat: rest packages, database

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
+dotenv.config();
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
 
 const start = async () => {
   try {
-    app.listen(PORT, () => console.log("Server started"));
+    await mongoose.connect(
+      process.env.DB_URL as any,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      } as any
+    );
+
+    app.listen(PORT, () => console.log(`Server started on ${PORT}`));
   } catch (error) {
     console.log(error);
   }
 };
+
+start();
