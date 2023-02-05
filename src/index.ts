3f91cd9eb6eef408104bfad1967b89eb948b6b28
Alexey <praesidios@gmail.com>
2023-02-05 19:17:15 +0300

    feat: project configs

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,28 @@
-import express from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
+mongoose.set('strictQuery', true);
 dotenv.config();
+
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
 const start = async () => {
   try {
     await mongoose.connect(
-      process.env.DB_URL as any,
+      process.env.DB_URL as string,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-      } as any
+      } as any,
     );
 
     app.listen(PORT, () => console.log(`Server started on ${PORT}`));
   } catch (error) {
+    console.log('ERRPR');
     console.log(error);
   }
 };
