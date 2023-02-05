cf87dbd3062e1e41ca4390f61867e51aeccf54ad
Alexey <praesidios@gmail.com>
2023-02-05 20:36:12 +0300

    feat: user controller

---
+++ src/controllers/user-controller.ts
@@ -0,0 +1,34 @@
+class UserController {
+  async registration(req: any, res: any, next: any) {
+    try {
+    } catch (e) {}
+  }
+
+  async login(req: any, res: any, next: any) {
+    try {
+    } catch (e) {}
+  }
+
+  async logout(req: any, res: any, next: any) {
+    try {
+    } catch (e) {}
+  }
+
+  async activate(req: any, res: any, next: any) {
+    try {
+    } catch (e) {}
+  }
+
+  async refresh(req: any, res: any, next: any) {
+    try {
+    } catch (e) {}
+  }
+
+  async getUsers(req: any, res: any, next: any) {
+    try {
+      res.json(['123', '23232', 'some test']);
+    } catch (e) {}
+  }
+}
+
+export default new UserController();
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import router from './router';
 
 mongoose.set('strictQuery', true);
 dotenv.config();
@@ -9,6 +12,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use('./api', router);
 
 const start = async () => {
   try {
